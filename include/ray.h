b2c8438ac43cd207caf4715f81218ee0b1dcd242
Tduncan13 <chico55_8008@yahoo.com>
2022-01-18 00:39:12 -0500

    initial commit

---
+++ include/ray.h
@@ -0,0 +1,27 @@
+#pragma once 
+
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+	public: 
+		ray() {}
+		ray(const point3& origin, const vec3& direction)
+			: m_orig(origin), m_dir(direction) 
+		{}
+
+		point3 origin() const { return m_orig; }
+		point3 direction() const { return m_dir; }
+
+		point3 at(double t) const {
+			return m_orig + t * m_dir;
+		}
+
+	public: 
+		point3 m_orig;
+		vec3 m_dir;
+};
+
+#endif
---
+++ include/sphere.h
@@ -0,0 +1,49 @@
+#pragma once 
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+	public: 
+		sphere() {}
+		sphere(point3 cen, double r) : m_center(cen), m_radius(r) {};
+
+		virtual bool hit(
+			const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+	public: 
+		point3 m_center;
+		double m_radius;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+	vec3 oc = r.origin() - m_center;
+	auto a = r.direction().length_squared();
+	auto half_b = dot(oc, r.direction());
+	auto c = oc.length_squared() - m_radius * m_radius;
+	
+	auto discriminant = half_b * half_b - a * c;
+	if (discriminant < 0) return false;
+	auto sqrtd = sqrt(discriminant);
+
+	// Find the nearest root that lies in the acceptatble range.
+	auto root = (-half_b - sqrtd) / a;
+	if (root < t_min || t_max < root) {
+		root = (-half_b + sqrtd) / a;
+		if(root < t_min || t_max < root)
+			return false;
+	}
+
+	rec.t = root;
+	rec.p = r.at(rec.t);
+	rec.normal = (rec.p - m_center) / m_radius;
+	vec3 outward_normal = (rec.p - m_center) / m_radius;
+	rec.set_face_normal(r, outward_normal);
+
+	return true;
+}
+
+#endif
